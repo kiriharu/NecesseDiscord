6535f502989469f1f7d8b2ccdbea49a5ccdfe5cd
kiriharu <kiriharu@yandex.ru>
2024-07-22 14:52:26 +0300

    POC

---
+++ src/main/java/necessediscord/NecesseDiscord.java
@@ -0,0 +1,13 @@
+package necessediscord;
+
+import necesse.engine.modLoader.annotations.ModEntry;
+
+@ModEntry
+public class NecesseDiscord {
+
+    public void init() {
+        System.out.println("working");
+    }
+
+
+}
---
+++ src/main/java/necessediscord/listeners/DiscordChatListener.java
@@ -0,0 +1,11 @@
+package necessediscord.listeners;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class DiscordChatListener extends ListenerAdapter {
+
+    public void onGuildMessageReceived(MessageReceivedEvent event) {
+
+    }
+}
