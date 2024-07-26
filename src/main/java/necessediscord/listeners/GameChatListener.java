4415c0f1a9e01b9b7345e935c3798506ddcd2559
kiriharu <kiriharu@yandex.ru>
2024-07-26 19:57:20 +0300

    FIX: Don't send player commands to discord

--- src/main/java/necessediscord/listeners/GameChatListener.java
+++ src/main/java/necessediscord/listeners/GameChatListener.java
@@ -12,6 +12,10 @@ public class GameChatListener extends GameEventListener<PacketChatMessageEvent> 
     public void onEvent(PacketChatMessageEvent event) {
         String player = NecesseDiscord.SERVER.getClient(event.packet.slot).getName();
         String msg = event.packet.gameMessage.translate();
+
+        // Don't send commands to chat
+        if (msg.startsWith("/")) return;
+
         MessageCreateData messageCreateData = MessageCreateData.fromContent(player + ": " + msg);
         NecesseDiscord.CHANNEL.sendMessage(messageCreateData).queue();
     }
